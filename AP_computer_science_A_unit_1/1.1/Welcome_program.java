5bcf7667ef28ec37b93889024c27a38fa0012630
Wal3ter-Rob0ert <aidynabenov20@gmail.com>
2025-11-10 10:04:34 +0500

    feat(unit1-1.1): add Welcome program

---
+++ AP_computer_science_A_unit_1/1.1/Welcome_program.java
@@ -0,0 +1,8 @@
+public class Welcome 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My name is Aidyn");
+        System.out.println("My favorite hobby is cycling");
+    } 
+}
