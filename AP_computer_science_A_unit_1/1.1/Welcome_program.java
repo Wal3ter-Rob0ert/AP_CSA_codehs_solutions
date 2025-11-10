0bccdf4bbf53b553ab1c482f2c38af7a9e4bd779
Wal3ter-Rob0ert <aidynabenov20@gmail.com>
2025-11-10 12:54:19 +0500

    Change name in Welcome_program to Aidyn

--- AP_computer_science_A_unit_1/1.1/Welcome_program.java
+++ AP_computer_science_A_unit_1/1.1/Welcome_program.java
@@ -2,7 +2,7 @@ public class Welcome
 {
     public static void main(String[] args)
     {
-        System.out.println("My name is Altair");
+        System.out.println("My name is Aidyn");
         System.out.println("My favorite hobby is cycling");
     } 
 }
