80fb75b95256f4cd8e18ac4e95b09f59aa4f83bf
Wal3ter-Rob0ert <aidynabenov20@gmail.com>
2025-11-11 11:04:04 +0500

    feat(unit1-1.2): add Team_rankings.java

---
+++ AP_computer_science_A_unit_1/1.2_Variables_and_data_type/Team_rankings.java
@@ -0,0 +1,36 @@
+public class TeamRanks
+{
+    public static void main(String[] args) {
+		String team1 = "Alabama";
+		String team2 = "Ohio State";
+		String team3 = "Florida State";
+		String team4 = "USC";
+		String team5 = "Clemson";
+		String team6 = "Penn State";
+		String team7 = "Oklahoma";
+		String team8 = "Maryland";
+		String team9 = "Wisconsin";
+		String team10 = "Michigan";
+		
+		System.out.print("1. ");
+		System.out.println(team1);
+		System.out.print("2. ");
+		System.out.println(team6);
+		System.out.print("3. ");
+		System.out.println(team8);
+		System.out.print("4. ");
+		System.out.println(team10);
+		System.out.print("5. ");
+		System.out.println(team4);
+		System.out.print("6. ");
+		System.out.println(team5);
+		System.out.print("7. ");
+		System.out.println(team7);
+		System.out.print("8. ");
+		System.out.println(team9);
+		System.out.print("9. ");
+		System.out.println(team3);
+		System.out.print("10. ");
+		System.out.println(team2);
+	}
+}
