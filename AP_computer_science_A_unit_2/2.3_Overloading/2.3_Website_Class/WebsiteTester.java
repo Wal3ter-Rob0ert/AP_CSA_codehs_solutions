2ceec01fec9fd7292d2a1a233980dc46f25d8957
Wal3ter-Rob0ert <aidynabenov20@gmail.com>
2025-11-13 16:57:20 +0500

    feat(unit2-2.3): add Website Class

---
+++ AP_computer_science_A_unit_2/2.3_Overloading/2.3_Website_Class/WebsiteTester.java
@@ -0,0 +1,16 @@
+public class WebsiteTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Website site1 = new Website();
+        
+        Website site2 = new Website("goodSchool", "edu");
+        
+        Website site3 = new Website("codehs", "com", 1000000);
+    
+        System.out.println(site1);
+        System.out.println(site2);
+        System.out.println(site3);
+    }
+}
