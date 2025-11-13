bc24a21d1695c93a3749ecf98ab1a7f34665d674
Wal3ter-Rob0ert <aidynabenov20@gmail.com>
2025-11-13 16:52:48 +0500

    feat(unit2-2.3): add Custom Pinata

---
+++ AP_computer_science_A_unit_2/2.3_Overloading/2.3_Custom_Piñatas/Pinata.java
@@ -0,0 +1,45 @@
+public class Pinata
+{
+    // Instance variables
+    private String candy;    // what kind of candy is inside
+    private String color;    
+    private String shape;
+    
+    // Constructor without parameters
+    public Pinata()
+    {
+        candy = "hard candy";
+        color = "rainbow";
+        shape = "donkey";
+    }
+    
+    // Add an overloaded constructor that allows the user
+    // to customize all of the instance variables
+    public Pinata(String candyType, String colorType, String shapeType)
+    {
+        candy = candyType;
+        color = colorType;
+        shape = shapeType;
+    }
+    // Add an overloaded constructor that allows the user
+    // to customize the color and shape
+    public  Pinata(String colorType, String shapeType)
+    {
+        candy = "hard candy";
+        color = colorType;
+        shape = shapeType;
+    }
+    // Add an overloaded constructor that allows the user
+    // to customize the candy
+    public Pinata(String candyType)
+    {
+        candy = candyType;
+        color = "rainbow";
+        shape = "donkey";
+    }
+    
+    public String toString()
+    {
+        return color + " " + shape + " pinata filled with " + candy;
+    }
+}
