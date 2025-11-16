1bc27766bc54843958a62bc031173038e86f74b3
Wal3ter-Rob0ert <aidynabenov20@gmail.com>
2025-11-16 15:37:23 +0500

    feat(unit2-2.5): add 2.5 Using the Point Class

---
+++ AP_computer_science_A_unit_2/2.5_Calling_a_void_method_with_parametrs/2.5_Using_the_Point_Class/PointTester.java
@@ -0,0 +1,20 @@
+public class PointTester
+{
+    public static void main(String[] args)
+    {
+        Point p = new Point(10, 5);
+        System.out.println(p);
+        p.move(3, 4);
+        
+        System.out.println(p);
+        // Make a new point here at position (2, 4)
+        Point q = new Point(2, 4);
+        // Then print it out
+        System.out.println(q);
+        // Move the point 5 units in the x direction and
+         q.move(5, 2);
+        // 2 units in the y direction
+        // Print out the point again to see that it moved
+        System.out.println(q);
+    }
+}
