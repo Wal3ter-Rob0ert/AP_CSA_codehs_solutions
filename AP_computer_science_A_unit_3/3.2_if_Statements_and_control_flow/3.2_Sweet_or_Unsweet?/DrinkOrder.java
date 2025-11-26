cc40d792f97677fca1e65e360850de2fc6339d7a
Wal3ter-Rob0ert <aidynabenov20@gmail.com>
2025-11-26 12:54:51 +0500

    feat(unit3-3.2): add Sweet or Unsweet?

---
+++ AP_computer_science_A_unit_3/3.2_if_Statements_and_control_flow/3.2_Sweet_or_Unsweet?/DrinkOrder.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DrinkOrder
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("What do you want to drink? ");
+        String drink = input.nextLine();
+        
+        System.out.println("How many teaspoons of sugar do you want? ");
+        int sugar = input.nextInt();
+        
+        System.out.println("\nConfirming your order. You wanted: ");
+        
+        if (sugar > 0) {
+            System.out.println(drink + " with sugar");
+        } else {
+            System.out.println(drink);
+
+            input.close();
+        }
+    }
+}
