883ef392827a29c3bb3b3d0b70f7ff9d508f5419
Wal3ter-Rob0ert <aidynabenov20@gmail.com>
2026-03-31 13:46:28 +0500

    feat: add new task

---
+++ AP_computer_science_A_unit_4/4.1_Iteration/4.1.8_ Divisibility/ExtractDigits.java
@@ -0,0 +1,19 @@
+public class ExtractDigits
+{
+    public static void main(String[] args)
+    {
+        extractDigits(2938724);
+        
+    }
+    
+    public static void extractDigits(int num)
+    {
+        while(num > 0)
+        {
+        int digit = num % 10;
+        System.out.println(digit);
+        num = num / 10;
+        
+        }
+    }
+}
