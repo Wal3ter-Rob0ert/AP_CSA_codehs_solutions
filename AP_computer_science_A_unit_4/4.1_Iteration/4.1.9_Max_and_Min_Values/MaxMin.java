cc4bde235e2dd545ac301130b4aff175a170fcf8
Wal3ter-Rob0ert <aidynabenov20@gmail.com>
2026-03-31 13:49:07 +0500

    feat: add new task

---
+++ AP_computer_science_A_unit_4/4.1_Iteration/4.1.9_Max_and_Min_Values/MaxMin.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class MaxMin
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+         
+        int number;
+        int max = 0;
+        int min = 0;
+        boolean first = true;
+        
+        while (true)
+        {
+            System.out.println("Enter a number or 0 to quit: ");
+            number = scanner.nextInt();
+            
+            if (number == 0)
+            {
+                break;
+            }
+            
+            if (first)
+            {
+                max = number;
+                min = number;
+                first = false;
+            }
+            else
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+                
+                if (number < min)
+                {
+                    min = number;
+                }
+            }
+            
+            System.out.println("Results so far:");
+            System.out.println("Largest number: " + max);
+            System.out.println("Smallest number: " + min);
+        }
+    }
+}
